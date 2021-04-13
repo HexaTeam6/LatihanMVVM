68799f8b528a111982ad553ca18702917095628b
hexateam6 <wahed.18051@mhs.its.ac.id>
2021-04-13 11:30:06 +0700

    selesai

---
+++ LatihanMVVM/ItemPenjualan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LatihanMVVM
+{
+    public class ItemPenjualan
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [StringLength(50)]
+        public string NamaBarang { get; set; }
+
+        public ItemPenjualan()
+        {
+            DiskonPersen = 0;
+        }
+
+        public int Jumlah { get; set; }
+
+        public decimal Harga { get; set; }
+
+        public decimal DiskonPersen { get; set; }
+
+        public decimal Total()
+        {
+            decimal total = Jumlah * Harga;
+            return total - (DiskonPersen / 100 * total);
+        }
+    }
+}
